f54bf7792f44aebe61424f23fb922b3a27c1ba36
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2021-09-10 20:16:08 -0300

    config ejs

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const server = express();
+
+const routes = require("./routes");
+
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "views"));
+
+server.use(routes);
+
+server.listen(3333, () => console.log("Server is Running"));
