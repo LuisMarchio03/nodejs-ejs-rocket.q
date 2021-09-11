36cdee17d9f7265802e6630fac2d20cc7133a463
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2021-09-11 07:58:36 -0300

    migration for ejs

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => res.render("index"));
+router.get("/create-pass", (req, res) => res.render("create-pass"));
+router.get("/room", (req, res) => res.render("room"));
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const routes = require("./routes");
 server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "views"));
 
+server.use(express.static("public"));
 server.use(routes);
 
 server.listen(3333, () => console.log("Server is Running"));
